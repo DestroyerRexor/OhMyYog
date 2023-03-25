bbbfd2e39a0efc1510eed9ca3687e4401f94e4a6
DestroyerRexor <edgardejesus_01@hotmail.com>
2023-03-25 14:46:37 -0600

    Implement empty activity commit

---
+++ app/src/main/java/com/example/evidenciafinalapp/CartViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.evidenciafinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_view);
+    }
+}
--- app/src/main/java/com/example/evidenciafinalapp/MainActivity.java
+++ app/src/main/java/com/example/evidenciafinalapp/MainActivity.java
@@ -12,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        //Activamos la pantalla completa para esta activity
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Hacemos el cambio de actividad, de la MainActivity a la ProductListActivity
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
