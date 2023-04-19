661e04989f7b28ef2753dbae0b2915c9835eeb6d
OlivaDJohnATOS <jonathan.oliva.external@atos.net>
2023-04-18 20:29:58 -0500

    Added "Shopping Cart" buttons functionality, and minor visual fixes

--- app/src/main/java/com/example/evidenciafinalapp/CartViewActivity.java
+++ app/src/main/java/com/example/evidenciafinalapp/CartViewActivity.java
@@ -3,8 +3,12 @@ package com.example.evidenciafinalapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +22,10 @@ public class CartViewActivity extends AppCompatActivity {
     private CartAdapter cartAdapter;
     private int total = 0;
     private List<CartItem> cartItems = ShoppingCartSingleton.getInstance().getArray();
+    ImageView cartImageView;
+    ImageView returnArrowImageView;
+    Button keepShopping;
+    Button finishOrder;
 
 
     @Override
@@ -26,6 +34,19 @@ public class CartViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart_view);
 
         final TextView priceView = (TextView) findViewById(R.id.totalPrice);
+
+        cartImageView = findViewById(R.id.cartImageView);
+        returnArrowImageView = findViewById(R.id.returnArrowImageView);
+        keepShopping = findViewById(R.id.keepShopping);
+        finishOrder = findViewById(R.id.finishOrder);
+
+        cartImageView.setImageAlpha(0);
+        returnArrowImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         for (CartItem item : cartItems) {
             total += (item.quantity * item.price);
@@ -40,6 +61,22 @@ public class CartViewActivity extends AppCompatActivity {
 
         cartAdapter = new CartAdapter(cartItems, position -> deleteItem(position));
         cartRecyclerView.setAdapter(cartAdapter);
+
+        keepShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shoppingList = new Intent(getApplicationContext(), ProductListActivity.class);
+                startActivity(shoppingList);
+            }
+        });
+
+        finishOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resumeDelivery = new Intent(getApplicationContext(), ResumeDeliveryActivity.class);
+                startActivity(resumeDelivery);
+            }
+        });
 
         // TODO: Add items to cartItems list and update cartAdapter
     }
--- app/src/main/java/com/example/evidenciafinalapp/ResumeDeliveryActivity.java
+++ app/src/main/java/com/example/evidenciafinalapp/ResumeDeliveryActivity.java
@@ -2,13 +2,27 @@ package com.example.evidenciafinalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResumeDeliveryActivity extends AppCompatActivity {
 
+    Button keepShopping;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_delivery);
+
+        keepShopping = findViewById(R.id.keepShopping);
+
+        keepShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shoppingList = new Intent(getApplicationContext(), ProductListActivity.class);
+                startActivity(shoppingList);
+            }
+        });
     }
 }
